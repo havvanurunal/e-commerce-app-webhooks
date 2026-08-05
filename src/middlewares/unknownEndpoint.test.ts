48eeb5cadd4eadc661697d6da608a439d5231e28
Havva Nur Unal <havvanurunal207@icloud.com>
2026-08-05 10:25:58 +0300

    test: add mocks for webhooks controller and stripe in unknownEndpoint tests

--- src/middlewares/unknownEndpoint.test.ts
+++ src/middlewares/unknownEndpoint.test.ts
@@ -1,6 +1,24 @@
 import request from 'supertest'
 import app from '../app'
 
+jest.mock('../resources/webhooks/controller', () => {
+  return {
+    __esModule: true,
+    default: {
+      receiveUpdates: (_req, res) => res.status(200).send(),
+    },
+  }
+})
+
+jest.mock('../common/stripe', () => ({
+  stripe: {
+    webhooks: {
+      constructEvent: jest.fn(),
+    },
+  },
+  STRIPE_ENDPOINT_SECRET: 'test-secret',
+}))
+
 describe('unknownEndpoint middleware', () => {
   it('returns 404 for unknown endpoints', async () => {
     const response = await request(app).get('/this-does-not-exist')
